ffdeda3f4f59f0936ea257288b0b23186def59cb
anujdivesh <divesha@spc.int>
2024-08-20 12:42:40 +1200

    latest2

--- pop_middleware/settings.py
+++ pop_middleware/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ta9qo@#2a9t0vr97)l@n63w8dvnks4g_c)287-#c%vdo-16l*x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS=['https://*.spc.int',"http://localhost:8089"]
 
